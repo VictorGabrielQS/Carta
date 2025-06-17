c0aaa081bb4e627d9d9cb3dda6329e85b110169b
VictorGabrielQS <victor.git24@gmail.com>
2025-06-17 16:31:30 -0300

    quase concluido

--- script.js
+++ script.js
@@ -53,3 +53,48 @@ botaoFechar.addEventListener('click', () => {
     elementoCoracao.style.display = 'none'
   }, 500)
 })
+
+
+
+
+
+//Galeria : 
+  const slides = document.querySelectorAll(".slide");
+  const prevBtn = document.querySelector(".prev");
+  const nextBtn = document.querySelector(".next");
+  let index = 0;
+
+  function mostrarSlide(i) {
+    slides.forEach(slide => slide.classList.remove("ativa"));
+    slides[i].classList.add("ativa");
+  }
+
+  function proximoSlide() {
+    index = (index + 1) % slides.length;
+    mostrarSlide(index);
+  }
+
+  function slideAnterior() {
+    index = (index - 1 + slides.length) % slides.length;
+    mostrarSlide(index);
+  }
+
+  nextBtn.addEventListener("click", proximoSlide);
+  prevBtn.addEventListener("click", slideAnterior);
+
+  setInterval(proximoSlide, 3000); // troca automática a cada 3 segundos
+
+
+
+
+
+    const chuva = document.querySelector('.chuva-de-coracoes');
+  setInterval(() => {
+    const coracao = document.createElement('div');
+    coracao.textContent = '❤';
+    coracao.classList.add('cora');
+    coracao.style.left = Math.random() * 100 + 'vw';
+    coracao.style.fontSize = (Math.random() * 10 + 10) + 'px';
+    chuva.appendChild(coracao);
+    setTimeout(() => coracao.remove(), 6000);
+  }, 300);
